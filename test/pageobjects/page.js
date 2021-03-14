be1f243a35ee46f83c29264069bcdde8259d8b4c
mpdaroy <42019183+mpdaroy@users.noreply.github.com>
2021-03-14 16:18:58 +0800

    Update page.js

--- test/pageobjects/page.js
+++ test/pageobjects/page.js
@@ -3,9 +3,6 @@
 * that is shared across all page objects
 */
 class Page {
-    constructor(name){
-        this.name = name
-    }
     /**
     * Opens a sub page of the page
     * @param path path of the sub page (e.g. /path/to/page.html)
